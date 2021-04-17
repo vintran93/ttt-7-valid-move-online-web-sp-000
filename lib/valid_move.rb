c21696320790776e8fd1f6ccb3d27314d6a4cd42
Vincent Tran <vincentuong@yahoo.com>
2021-04-17 04:38:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if index.between?(0, 9) && if !position_taken?(board, index)
     return true
-  else if position_taken?(board, index) 
+  elsif position_taken?(board, index) 
     return false
   else
     return false
